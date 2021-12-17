e7f971ceae67eaebe5ade48d699214fad35bbe18
kutlu <kutlumeydanli@gmail.com>
2021-12-17 22:55:47 +0300

    Third commit: I still keep learning this.

--- src/Admin.java
+++ src/Admin.java
@@ -15,7 +15,7 @@ public class Admin extends Employee implements IViewPatients {
     }
 
     public void answerRequest() {
-
+        System.out.println("My second change.");
     }
 
     public void viewEmployee() {
